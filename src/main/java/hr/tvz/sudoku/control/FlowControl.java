2ec43143982f40fe79ddd641313d4fdcdaadb8d8
josko-heh <josko.jurcevic@2e-systems.com>
2023-03-31 17:42:23 +0200

    formatting

--- src/main/java/hr/tvz/sudoku/control/FlowControl.java
+++ src/main/java/hr/tvz/sudoku/control/FlowControl.java
@@ -8,14 +8,13 @@ import javafx.stage.Stage;
 public class FlowControl {
 	
 	public static void initialize(Stage stage) {
-		
 		Settings settings = new Settings();
 		settings.setOnConfirm(event -> {
 			Generator generator = new Generator(settings.getSize(), settings.getEmptyBoxes());
 			stage.setScene(new Scene(generator.generateBoard()));
 		});
-		
 		Scene initialScene = new Scene(settings.construct());
+		
 		stage.setTitle("Sudoku");
 		stage.setScene(initialScene);
 		stage.show();
