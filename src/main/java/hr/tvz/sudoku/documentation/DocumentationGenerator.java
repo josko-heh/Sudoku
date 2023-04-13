02d07517e1169e48243b2e3ef58b6c0187788738
josko-heh <josko.jurcevic@2e-systems.com>
2023-04-13 22:02:18 +0200

    generate constructors, methods, fields and annotations

--- src/main/java/hr/tvz/sudoku/documentation/DocumentationGenerator.java
+++ src/main/java/hr/tvz/sudoku/documentation/DocumentationGenerator.java
@@ -1,14 +1,18 @@
 package hr.tvz.sudoku.documentation;
 
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -22,8 +26,10 @@ public class DocumentationGenerator {
 		Path start = Paths.get(System.getProperty("user.dir"));
 
 		try (Stream<Path> stream = Files.walk(start);
-			 Writer writer = new FileWriter("documentation.txt")) 
+			 Writer writer = new FileWriter("documentation.html")) 
 		{
+			writer.write(getHeader("Documentation", 1));
+			
 			stream.filter(path -> {
 				String fileName = path.getFileName().toString();
 				return fileName.endsWith(".class") && !fileName.startsWith("module-info");
@@ -32,7 +38,7 @@ public class DocumentationGenerator {
 				path.forEach(p -> pathElements.add(p.toString()));
 
 				try {
-					writer.write(path.getFileName().toString() + "\n");
+					writer.write(getHtml(path.getFileName().toString(), "h3"));
 	
 					String packageName = "";
 					boolean startPackage = false;
@@ -46,12 +52,58 @@ public class DocumentationGenerator {
 							startPackage = true;
 					}
 	
-					Class myClass = Class.forName(packageName.substring(0, packageName.length() - 7));
-					Constructor[] constructors = myClass.getConstructors();
-	
-					for (Constructor c : constructors) {
-						writer.write("Constructor: " + c.getName() + "\n");
+					Class clazz = Class.forName(packageName.substring(0, packageName.length() - 7));
+
+					Class[] interfaces = clazz.getInterfaces();
+					if (interfaces.length != 0) {
+						writer.write(getHeader("Interfaces", 4));
+						for (Class interfacee : interfaces) {
+							writer.write(getHtml(interfacee.getName(), "span"));
+						}
 					}
+					
+					Class superclass = clazz.getSuperclass();
+					if (superclass != null) {
+						writer.write(getHeader("Superclass", 4));
+						writer.write(getHtml(superclass.getName(), "span"));
+					}
+
+					List<Constructor> nonPrivateConstructors = Arrays.stream(clazz.getDeclaredConstructors())
+							.filter(method -> !Modifier.isPrivate(method.getModifiers()))
+							.toList();
+					if (!nonPrivateConstructors.isEmpty())
+						writer.write(getHeader("Constructors", 4));
+					for (Constructor constructor : nonPrivateConstructors) {
+						writer.write(getHtml(constructor.toGenericString(), "span"));
+					}
+					
+					Annotation[] annotations = clazz.getDeclaredAnnotations();
+					if (annotations.length != 0) {
+						writer.write(getHeader("Annotations", 4));
+						for (Annotation annotation : annotations) {
+							writer.write(getHtml(annotation.toString(), "span"));
+						}
+					}
+
+					List<Field> nonPrivateFields = Arrays.stream(clazz.getDeclaredFields())
+							.filter(method -> !Modifier.isPrivate(method.getModifiers()))
+							.toList();
+					if (!nonPrivateFields.isEmpty())
+						writer.write(getHeader("Fields", 4));
+					for (Field field : nonPrivateFields) {
+						writer.write(getHtml(field.toGenericString(), "span"));
+					}
+					
+					List<Method> nonPrivateMethods = Arrays.stream(clazz.getDeclaredMethods())
+							.filter(method -> !Modifier.isPrivate(method.getModifiers()))
+							.toList();
+					if (!nonPrivateMethods.isEmpty())
+						writer.write(getHeader("Methods", 4));
+					for (Method method : nonPrivateMethods) {
+						writer.write(getHtml(method.toGenericString(), "span"));
+					}
+
+					writer.write("<hr>" + "\n");
 				} catch (ClassNotFoundException | IOException e) {
 					throw new RuntimeException(e);
 				}
@@ -61,6 +113,14 @@ public class DocumentationGenerator {
 		}
 
 		showMessage("Generate documentation", "Documentation generated successfully!", "");
+	}
+
+	private static String getHtml(String content, String element) {
+		return "<" + element + ">" + content + "</" + element + "><br>\n";
+	}
+	
+	private static String getHeader(String content, int headerLevel) {
+		return "<h" + headerLevel + ">" + content + "</h" + headerLevel + ">\n";
 	}
 
 	private static void handleFail(Exception e) {
